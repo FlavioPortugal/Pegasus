719a77557fa11b6f900f321e6365d788ee206acc
Flavio Portugal <FlavioPortugal>
2018-09-26 22:52:31 -0300

    Adiciona AlunoDAO

--- src/br/com/pegasus/dao/AlunoDAO.java
+++ src/br/com/pegasus/dao/AlunoDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import br.com.pegasus.beans.Usuario;
 import br.com.pegasus.conexao.Conexao;
 
 public class AlunoDAO {
@@ -15,6 +16,19 @@ public class AlunoDAO {
 	
 	public AlunoDAO() throws Exception {
 		con = Conexao.conectar();
+	}
+	
+	public Usuario consultarBoletim(String codigo) throws Exception {
+		Usuario user = new Usuario();
+		stmt = con.prepareStatement(
+				"SELECT * FROM T_PEG_ALUNO Aluno INNER JOIN T_PEG_FREQUENCIA freq on Aluno.CD_ALUNO = freq.CD_ALUNO WHERE CD_ALUNO = ?");
+		stmt.setString(1, codigo);		
+		rs = stmt.executeQuery();
+		if (rs.next()) {
+			user.setLogin(rs.getString("NR_LOGIN"));
+			user.setSenha(rs.getString("DS_SENHA"));
+		}
+		return user;
 	}
 	
 	public void fechar() throws Exception {
